5131fb299f702b0ad21bd948a3293a7992844bc4
Xursandbek Ibadullayev <xursandbek1014@gmail.com>
2023-03-14 11:48:04 +0500

    debug

--- fileSystem/settings.py
+++ fileSystem/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG') == 'True'
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 ALLOWED_HOSTS = ['*']
